4f248994e4e0581d6c46dbf6adf078c4d1943396
thisantm <thiago.sant.mach@gmail.com>
2025-01-09 11:35:30 -0300

    refactor main.py for code clarity and style consistency

--- main.py
+++ main.py
@@ -7,36 +7,43 @@ def main():
     letters_list.sort(reverse=True, key=sort_on)
     print(f"--- Begin report of {book_path} ---")
     print(f"{words} words found in the document")
+
     for items in letters_list:
         print(f"The '{items["char"]}' character was found {items["num"]} times")
     print("--- End report ---")
 
+
 def sort_on(dict):
     return dict["num"]
+
 
 def dict_to_list(letters):
     dict_letters = []
     for letter in letters:
-        if(letter.isalpha()):
+        if letter.isalpha():
             dict_letters.append({"char": letter, "num": letters[letter]})
     return dict_letters
+
 
 def read_book(path):
     with open(path) as f:
         return f.read()
-    
+
+
 def word_count(text):
     words = text.split()
     return len(words)
+
 
 def letter_count(text):
     text_lower = text.lower()
     letters = {}
     for letter in text_lower:
-        if(letter in letters):
+        if letter in letters:
             letters[letter] += 1
         else:
             letters[letter] = 1
     return letters
-        
+
+
 main()
